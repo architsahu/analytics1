824ef1027ed1b6f2f8854bbb26768720685c01ef
unknown <archit.pgp18101@iimkashipur.ac.in>
2018-10-24 00:44:55 +0530

    push

---
+++ packages.R
@@ -0,0 +1,97 @@
+# Packages Management in R
+
+#List avl packages
+library()
+
+#Total Avl Packages in net
+nrow(available.packages())
+
+#Install Package - single & multiple
+install.packages('amap')
+install.packages(c('foreign', 'haven'))
+
+#Load package
+library(amap)
+
+#Find functions in package (Tab)
+library(help=amap)
+
+#Help wrt a package(Pane)
+help(package='amap')  #see on right side pane
+
+
+#Unload/ Detach Packages---
+search()
+detach(package:amap, unload = T) 
+search()
+
+
+#Check if library avl for use
+find.package("amap")
+require(amap)  #this will check and load
+
+#Remove Packages ----- uninstall 
+install.packages('amap')
+remove.packages("amap")
+require('amap')# check if unistalled
+find.package("amap")
+
+
+#Install Multiple Packages----
+easypackages::packages("plyr", "psych", "abc")
+easypackages::libraries("plyr", "psych", "tm")
+
+
+#Installing from other repositories
+install.packages("CHAID", repos="http://R-Forge.R-project.org")
+#github
+require(devtools)
+install_github("Espanta/lubripack")
+
+#use lubripack package to install multiple package
+library(lubripack)
+lubripack("plyr", "psych", "tm", "quantmod")
+
+#Check if package is avl for download from repository
+pack = available.packages()
+pack["ggplot2","Depends"]
+pack["amap","Depends"]
+
+#version which this ggplot2 requires
+
+
+
+
+
+#This will unload/Detach all  packages
+library(mise)
+search()
+mise(pkgs=T)
+search()
+
+
+
+# list all packages where an update is available
+old.packages()
+
+# update all available packages
+update.packages()
+
+# update, without prompts for permission/clarification
+update.packages(ask = FALSE)
+
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+
+Press h to open a hovercard with more details.
